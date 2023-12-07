5ecac6a25673034d6800e2792afdae426c7f8171
Thibaut Perrin <103429802+tperrindell@users.noreply.github.com>
2023-12-07 15:29:17 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Kasm Workspaces',
+    description: 'Kasm Workspaces for Mtp Lab env.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://github.com/tperrindell/kasm-registry/',
+    contactUrl: 'none',
   },
   reactStrictMode: true,
   swcMinify: true,
